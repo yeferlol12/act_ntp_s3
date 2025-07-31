cd1e655a172de8f60cf29df74e0de46f4ade84f9
yeferlol12 <ysantamaria@cesde.net>
2025-07-31 01:31:55 -0500

    actividad-10

---
+++ src/ejercicio_10.py
@@ -0,0 +1,8 @@
+total = 0
+while True:
+    text = input("Escribe palabras y para salir solo escribe fin: ")
+    if text == "fin":
+        break
+    total += 1    
+print("se han escrito esta cantidad de palabras:",total)
+
